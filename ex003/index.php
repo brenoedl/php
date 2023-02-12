6d4b1825ee00b03077c0d76a40c0a1d964c2a032
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-02-12 11:32:38 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis e constantes</title>
+</head>
+<body>
+    <h1>Variáveis e constantes</h1>
+    <?php 
+        $nome = "Breno";
+        $idade = 20;
+        const PAIS = "Brasil";
+        echo "Olá $nome você tem $idade anos e mora no pais ". PAIS;
+    ?>
+</body>
+</html>
