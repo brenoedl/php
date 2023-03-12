11f452f492e1f0f8800bc02f3202c87296bd6f66
Breno170 <96654144+Breno170@users.noreply.github.com>
2023-03-12 15:23:47 -0300

    mm

---
+++ exercicios/ex005/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style.css">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <header>
+        <h1>Tipos primitivos</h1>
+    </header>
+    <main>
+        <?php
+            $nome = 'Breno';
+            $idade = 21;
+            $peso = 55.8;
+            $namoramdo = false;
+            var_dump($nome);
+            var_dump($idade);
+            var_dump($namoramdo);
+            var_dump($peso);
+        ?>
+    </main>
+</body>
+</html>
